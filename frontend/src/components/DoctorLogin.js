e66970868858b649444b5f15ec31fdeec27f618f
Ashish1227 <ashishgatsy@gmail.com>
2024-02-07 20:29:58 +0530

    login doctor and nurse v1

---
+++ frontend/src/components/DoctorLogin.js
@@ -0,0 +1,9 @@
+// src/components/DoctorLogin.js
+import React from 'react';
+import Login from './Login';
+
+const DoctorLogin = () => {
+  return <Login userType="Doctor" />;
+};
+
+export default DoctorLogin;
---
+++ frontend/src/components/NurseLogin.js
@@ -0,0 +1,9 @@
+// src/components/NurseLogin.js
+import React from "react";
+import Login from "./Login";
+
+const NurseLogin = () => {
+  return <Login userType="Nurse" />;
+};
+
+export default NurseLogin;

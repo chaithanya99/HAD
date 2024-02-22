e7e5257158513ddacdcce23ffa1bc17eb4819f42
Dhanvi Medha <12015269+unbalancedvariance@users.noreply.github.com>
2024-02-22 21:41:01 +0530

    Update AbhaCreationController.java

--- backend/src/main/java/com/had/project5/controllers/AbhaCreationController.java
+++ backend/src/main/java/com/had/project5/controllers/AbhaCreationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.had.project5.services.ApiService;
@@ -38,28 +39,34 @@ public class AbhaCreationController {
     }
 
     @PostMapping("/generateOtp")
-    public void generateOtp(@RequestBody Map<String,String> req) throws Exception{
+    public ResponseEntity<Map<String, String>> generateOtp(@RequestBody Map<String,String> req) throws Exception{
         try {
             System.out.println("working fine");
-        String encryptedAadhaar;
+            System.out.println(req);
+            String encryptedAadhaar;
             encryptedAadhaar = encryption.encryptWithPublicKey(req.get("aadhaar"));
             Map<String,String> payload=new HashMap<>();
-            // payload.put("aadhaar",req.get("aadhaar"));
             payload.put("aadhaar",encryptedAadhaar);
             String jsonPayload=new ObjectMapper().writeValueAsString(payload);
-            // encryptedAadhaar=req.get("aadhaar");
-            // JSONObject obj=new JSONObject();
-            // obj.put("aadhaar", encryptedAadhaar);
-            String res=apiService.makePostRequest("https://healthidsbx.abdm.gov.in/api/v2/registration/aadhaar/generateOtp",jsonPayload);
-            System.out.println(res);
-        // System.out.println(apiService.makeGetRequest("/v1/auth/cert"));
+
+          String res=apiService.makePostRequest("https://healthidsbx.abdm.gov.in/api/v2/registration/aadhaar/generateOtp",jsonPayload);
+
+//            String res=apiService.catfact("https://catfact.ninja/fact");
+//            JSONObject jsonResponse= new JSONObject(res);
+//            System.out.println(jsonResponse.getString("fact"));
+//            Map<String, String> responseMap = new HashMap<>();
+//            responseMap.put("message", "Cat fact retrieved successfully");
+//            responseMap.put("catFact", jsonResponse.getString("fact"));
+//            return ResponseEntity.ok(responseMap);
+
+            JSONObject jsonResponse= new JSONObject(res);
+            Map<String, String> responseMap = new HashMap<>();
+            responseMap.put("txtId", jsonResponse.getString("txtId"));
+            return ResponseEntity.ok(responseMap);
         } catch (Exception e) {
             // TODO: handle exception
             throw e;
         }
-        
-        
-        
     }
     @PostMapping("/verifyAadhaarOTP")
     public void verifyAadhaarOTP(@RequestBody Map<String,String> req){

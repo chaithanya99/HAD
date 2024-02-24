e4bf92ec677215fd718c5d758f0fd53aeba7907d
Ashish1227 <ashishgatsy@gmail.com>
2024-02-24 14:22:44 +0530

    dropdown patient registration

--- frontend/src/components/ActionCard.js
+++ frontend/src/components/ActionCard.js
@@ -1,12 +1,17 @@
 // src/components/ActionCard.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ActionCard = () => {
   const navigate = useNavigate();
-
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
   const handleCardClick = (route) => {
     navigate(route);
+  };
+
+  const handleDropdownSelect = (route) => {
+    navigate(route);
+    setDropdownOpen(false);
   };
 
   return (
@@ -16,8 +21,23 @@ const ActionCard = () => {
         <div className="action-button" onClick={() => handleCardClick('/appointments')}>
           Appointments
         </div>
-        <div className="action-button" onClick={() => handleCardClick('/register-patient')}>
+        {/* <div className="action-button" onClick={() => handleCardClick('/register-patient')}>
           Register Patient
+        </div> */}
+        <div className="dropdown-container">
+          <div className="action-button dropdown-toggle" onClick={() => setDropdownOpen(!isDropdownOpen)}>
+            Register Patient
+          </div>
+          {isDropdownOpen && (
+            <div className="dropdown-options">
+              <div className="dropdown-option" onClick={() => handleDropdownSelect('/register-patient')}>
+                Create ABHA ID
+              </div>
+              <div className="dropdown-option" onClick={() => handleDropdownSelect('/register-patient-step2')}>
+                Register using ABHA ID
+              </div>
+            </div>
+          )}
         </div>
         <div className="action-button" onClick={() => handleCardClick('/reports')}>
           Reports

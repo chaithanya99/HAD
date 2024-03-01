cc10b14b538638778c65604814b6973238d10bd7
Ashish1227 <ashishgatsy@gmail.com>
2024-03-01 14:47:41 +0530

    file organisation and theme change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import DoctorLogin from './components/DoctorLogin';
-import NurseLogin from './components/NurseLogin';
 import Login1 from './components/Login1';
 import Login2 from './components/Login2';
 import Dashboard from './components/Dashboard';
-import PatientRegistration from './components/PatientRegistration';
+import PatientRegistration from './components/nurse_components/PatientRegistration';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -15,8 +13,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/doctor-login" element={<DoctorLogin />} />
-        <Route path="/nurse-login" element={<NurseLogin />} />
         <Route path="/login" element={<Login2/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/register-patient" element={<PatientRegistration/>}/>

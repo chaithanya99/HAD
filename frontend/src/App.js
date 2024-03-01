833084d071fefbaf9745f827df30305005a6d3f5
Ashish1227 <ashishgatsy@gmail.com>
2024-03-01 12:54:51 +0530

    login page and user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,14 @@
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-// src/App.js
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import DoctorLogin from './components/DoctorLogin';
 import NurseLogin from './components/NurseLogin';
 import Login1 from './components/Login1';
+import Login2 from './components/Login2';
 import Dashboard from './components/Dashboard';
 import PatientRegistration from './components/PatientRegistration';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -40,10 +17,11 @@ const App = () => {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/doctor-login" element={<DoctorLogin />} />
         <Route path="/nurse-login" element={<NurseLogin />} />
-        <Route path="/login" element={<Login1/>}/>
+        <Route path="/login" element={<Login2/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/register-patient" element={<PatientRegistration/>}/>
       </Routes>
+      <ToastContainer/>
     </Router>
   );
 };

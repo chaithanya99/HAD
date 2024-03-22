e4822866f97dab2746183c4dcfc5c9108565ba2a
unbalancedvariance <dhanvimedha@gmail.com>
2024-03-23 03:51:00 +0530

    minor changes

--- backend/src/main/java/com/had/project5/controllers/CallBackController.java
+++ backend/src/main/java/com/had/project5/controllers/CallBackController.java
@@ -1,4 +1,30 @@
 package com.had.project5.controllers;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CallBackController {
+import javax.crypto.Cipher;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.had.project5.services.ApiService;
+import com.had.project5.services.Encryption;
+import com.had.project5.services.JwtService;
+
+@RestController
+
+public class CallBackController{
+    @CrossOrigin(origins = "*")
+    @PostMapping("v0.5/users/auth/fetch-modes")
+    public void getreq(@RequestBody Map<String,String> req) {
+        System.out.print(req);
+    }
 }
--- backend/src/main/java/com/had/project5/entities/Patient.java
+++ backend/src/main/java/com/had/project5/entities/Patient.java
@@ -1,4 +1,25 @@
 package com.had.project5.entities;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 
 public class Patient {
+    private String id;
+    private String abha_number;
+    private String abha_address;
+    private String name;
+    private String gender;
+    private String yearOfBirth;
+    private String monthOfBirth;
+    private String dayOfBirth;
+    private Address address;
 }
+
+
+// How to set ABHA Address by patient.

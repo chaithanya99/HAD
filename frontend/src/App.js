bdeb0b58fea929322acdf78e1e804fc01302e2b9
Tejdeep2003 <tejdeepgutta03@gmail.com>
2024-03-29 17:13:25 +0530

    Login page first update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login1 from './components/Login1';
-import Login2 from './components/Login2';
+// import Login1 from './components/Login1';
+// import Login2 from './components/Login2';
 import Dashboard from './components/Dashboard';
-import PatientRegistration from './components/nurse_components/PatientRegistration';
+// import PatientRegistration from './components/nurse_components/PatientRegistration';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Login from './components/Login_Dashboard/Login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login2/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/register-patient" element={<PatientRegistration/>}/>
+        {/* <Route path="/register-patient" element={<PatientRegistration/>}/> */}
       </Routes>
       <ToastContainer/>
     </Router>
